b7c8e2ea7b368c382c32294dd360c1986e10026d
liMin <1013880299@qq.com>
2019-11-19 21:04:37 +0800

    chore:表单插件

---
+++ eledemo/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+  
+    <Formitem></Formitem>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Formitem from '../components/Form.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Formitem
+  },
+  data() {
+      return {
+        
+      }
+  }
+}
+</script>
