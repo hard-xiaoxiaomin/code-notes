b3f4b3f7a43aee631cce3cf9d80fa8413d16493a
liMin <1013880299@qq.com>
2020-04-12 17:52:21 +0800

    优化头部功能

--- react/search/src/App.js
+++ react/search/src/App.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
-import { Provider, observer, inject } from 'mobx-react';
-import store from './stroe';
 import './app.scss';
 import './iconfont/iconfont.css'
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputValue : '',
-            list: JSON.parse(localStorage.getItem('list')) || [],
-        }
+const searchKeyword = 'searchKeyword';// 搜索记录本地存储key
+
+export default class App extends Component {
+    state = {
+        inputValue : '',
+            searchHistory: this.searchKeyword ? this.searchKeyword.split(',').slice(0, 10) : []
     }
+
+    get searchKeyword() {
+        return localStorage.getItem(searchKeyword);
+    }
+
    //监听输入框的值
-    onChange (event) {
+    onChange = (event) => {
         this.setState({
             inputValue :event.target.value
         })
     }
     //判断输入框后清空按钮
-    closeInput (event) {
+    clearInput = (event) => {
         this.setState({
             inputValue : ''
         })
     }
     //提交按钮
-    handleIn () {
-        this.state.list.push(this.state.inputValue)
-    
-        localStorage.setItem('list',JSON.stringify(this.state.list));
+    handleInput = () => {
+        const {
+            inputValue
+        } = this.state;
+        const searchHistory = this.searchKeyword;
+
+        // // 没有搜索内容，此时为取消按钮
+        // if (inputValue === '') {
+        //     history.back();
+        //     return;
+        // }
+
+        // 如果本地存储已经有值，则应该采用追加写入
+        searchHistory ? localStorage.setItem(searchKeyword, `${inputValue},${searchHistory}`) : localStorage.setItem(searchKeyword, inputValue);
 
         this.setState({
-            inputValue : '',
-        })
+            isEndSearch: true,
+            searchHistory: this.searchKeyword.split(',').slice(0, 10)
+        });
 
     }
     
@@ -40,36 +53,44 @@ class App extends Component {
         const icon = (
             <i 
                 className="iconfont close-btn"
-                onClick={this.closeInput.bind(this)}
+                onClick={this.clearInput.bind(this)}
             >
                 &#xe651;
             </i>
         )
+
+        const {inputValue, searchHistory} = this.state;
         
         return (
-            <div className="search-wrap">
-                <div className="search-item">
-                    <i className="iconfont">&#xe63c;</i>
-                    <input 
-                        type="text" 
-                        placeholder="请输入" 
-                        value={this.state.inputValue}
-                        onChange = {this.onChange.bind(this)}
-                    />
-                    {(this.state.inputValue !== "") && icon}
+            <div>
+                <div className="search-wrap">
+                    <div className="search-item">
+                        <i className="iconfont">&#xe63c;</i>
+                        <input 
+                            type="text" 
+                            placeholder="请输入" 
+                            value={inputValue}
+                            onChange = {this.onChange}
+                        />
+                        {(inputValue !== "") && icon}
+                    </div>
+                    <button onClick={this.handleInput} className="btn-handle">添加</button>
                 </div>
-                <button onClick={this.handleIn.bind(this)}>添加</button>
-                <div className="content-wrap">
-                    {
-                        this.state.list.map((i, index) => {
-                            return (
-                                <p key={index}>{i}</p>
-                            )
-                        })
-                    }
-                </div>
+                <dl className="content-wrap">
+                    <dt>
+                        <span>最近搜索记录</span>
+                    </dt>
+                    <dd> 
+                        {
+                            searchHistory.map((i, index) => {
+                                return (
+                                    <p key={index}>{i}</p>
+                                )
+                            })
+                        }
+                    </dd>
+                </dl>
             </div>
         );
     }
 }
-export default App;
